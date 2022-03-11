9bb2be956d8b856d3d606bcb97290db6f2b7a264
Albert Ragány-Németh <raganyalbi@gmail.com>
2022-03-11 20:48:23 +0100

    Rename samples to console, remove console images except one

--- MaterialColorUtilities.Console/Program.cs
+++ MaterialColorUtilities.Console/Program.cs
@@ -8,7 +8,7 @@ using System.Reflection;
 // Generate seed color from an image:
 // Load the image into an int[].
 // The image is stored in an embedded resource, and then decoded and resized using SkiaSharp.
-string imageResourceId = "MaterialColorUtilities.Samples.Assets.5_wallpaper.webp";
+string imageResourceId = "MaterialColorUtilities.Console.Resources.wallpaper.webp";
 using Stream resourceStream = Assembly.GetExecutingAssembly().GetManifestResourceStream(imageResourceId)!;
 SKBitmap bitmap = SKBitmap.Decode(resourceStream).Resize(new SKImageInfo(112, 112), SKFilterQuality.Medium);
 int[] pixels = bitmap.Pixels.Select(p => (int)(uint)p).ToArray();
