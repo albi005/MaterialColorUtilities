22830f4ce5ef2867f952f13a32ce7ec641580941
Albert Ragány-Németh <raganyalbi@gmail.com>
2022-04-23 18:58:36 +0200

    Add copyright/license headers

--- MaterialColorUtilities/Score/Scorer.cs
+++ MaterialColorUtilities/Score/Scorer.cs
@@ -1,4 +1,19 @@
-﻿using MaterialColorUtilities.ColorAppearance;
+﻿// Copyright 2021 Google LLC
+// Copyright 2021-2022 project contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using MaterialColorUtilities.ColorAppearance;
 using MaterialColorUtilities.Utils;
 
 namespace MaterialColorUtilities.Score;
