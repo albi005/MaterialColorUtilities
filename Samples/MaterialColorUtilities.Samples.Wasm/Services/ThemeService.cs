4d25f9f31aceb54403796c135f26913922cbc398
Albert Ragány-Németh <raganyalbi@gmail.com>
2022-05-22 21:56:38 +0200

    Add Blazor WASM sample (#15)
    * Add basic Blazor WASM code
    
    * Add HCT graph to Blazor sample
    
    * Add workflow for deploying to Github Pages
    
    * Add MudBlazor, theming and seed selection
    
    * Add SeedColorSelector, display current color on ColorPlot

---
+++ Samples/MaterialColorUtilities.Samples.Wasm/Services/ThemeService.cs
@@ -0,0 +1,52 @@
+﻿using MaterialColorUtilities.Palettes;
+using MaterialColorUtilities.Samples.Wasm.Extensions;
+using MudBlazor;
+using MudBlazor.Utilities;
+
+namespace MaterialColorUtilities.Samples.Wasm.Services;
+
+public class ThemeService
+{
+    private int _seed = Score.Scorer.Default;
+
+    public ThemeService() => Apply();
+
+    public int Seed
+    {
+        get => _seed;
+        set
+        {
+            _seed = value;
+            Apply();
+        }
+    }
+
+    public void Apply()
+    {
+        CorePalette corePalette = new(Seed);
+        AppScheme<MudColor> lightScheme = new LightAppSchemeMapper()
+            .Map(corePalette)
+            .ConvertTo(IntExtensions.ToMudColor);
+        MudTheme.Palette = new()
+        {
+            Primary = lightScheme.Primary,
+            Secondary = lightScheme.Secondary,
+            Tertiary = lightScheme.Tertiary,
+            Background = lightScheme.Background,
+            AppbarBackground = lightScheme.Elevation2,
+            AppbarText = lightScheme.OnBackground,
+            DrawerBackground = lightScheme.Elevation1,
+            Surface = lightScheme.Elevation1,
+        };
+        ThemeChanged?.Invoke(this, EventArgs.Empty);
+    }
+
+    public MudTheme MudTheme { get; } = new()
+    {
+        ZIndex = new()
+        {
+            AppBar = 2000
+        }
+    };
+    public event EventHandler ThemeChanged;
+}
---
+++ Samples/MaterialColorUtilities.Samples.Wasm/Shared/HctSliders.razor.cs
@@ -0,0 +1,30 @@
+﻿using MaterialColorUtilities.ColorAppearance;
+using MaterialColorUtilities.Samples.Wasm.Services;
+
+namespace MaterialColorUtilities.Samples.Wasm.Shared
+{
+    public partial class HctSliders 
+    {
+        private double _hue;
+        private double _chroma;
+        private double _tone;
+
+        double Hue { get => _hue; set { if (_hue == value) return; _hue = value; HctChanged(); } }
+        double Chroma { get => _chroma; set { if (_chroma == value) return; _chroma = value; HctChanged(); } }
+        double Tone { get => _tone; set { if (_tone == value) return; _tone = value; HctChanged(); } }
+
+        protected override void SetFromSeed(int seed)
+        {
+            Hct hct = Hct.FromInt(seed);
+            _hue = hct.Hue;
+            _chroma = hct.Chroma;
+            _tone = hct.Tone;
+        }
+
+        void HctChanged()
+        {
+            Hct hct = Hct.From(Hue, Chroma, Tone);
+            ThemeService.Seed = hct.ToInt();
+        }
+    }
+}
