dfc4f26db2d2a1116aff2bc5edbb6f0bce5df28d
Albert Ragány-Németh <raganyalbi@gmail.com>
2022-05-26 23:20:40 +0200

    Add labels

--- Samples/MaterialColorUtilities.Samples.Wasm/Shared/ColorPlot.razor.cs
+++ Samples/MaterialColorUtilities.Samples.Wasm/Shared/ColorPlot.razor.cs
@@ -12,7 +12,8 @@ namespace MaterialColorUtilities.Samples.Wasm.Shared
         private SKCanvasView view;
         private double x;
         private double y;
-        private SKPaint paint = new() { Color = SKColors.White, Style = SKPaintStyle.Stroke, StrokeWidth = 2 };
+        private readonly SKPaint paint = new() { Color = SKColors.White, Style = SKPaintStyle.Stroke, StrokeWidth = 2 };
+        private readonly SKPaint textPaint = new() { Color = SKColors.Gray };
 
         /// <summary>
         /// The width of the bitmap that will be generated.
@@ -37,14 +38,26 @@ namespace MaterialColorUtilities.Samples.Wasm.Shared
         public void OnPaintSurface(SKPaintSurfaceEventArgs args)
         {
             var canvas = args.Surface.Canvas;
-            canvas.DrawBitmap(bitmap, 0, 0);
-            canvas.DrawCircle((float)x, (float)y, 4, paint);
+            canvas.DrawBitmap(bitmap, 20, 20);
+            canvas.DrawCircle((float)x + 20, (float)y + 20, 6, paint);
+            canvas.DrawText("0", 2, 10, textPaint);
+            canvas.DrawText("Hue", 200, 10, textPaint);
+            canvas.DrawText("360", 420, 10, textPaint);
+            canvas.DrawText("100", 2, 234, textPaint);
+
+            canvas.RotateDegrees(90);
+            canvas.DrawText("Tone", 110, -2, textPaint);
         }
 
         public void OnClick(MouseEventArgs args)
         {
-            x = args.OffsetX;
-            y = args.OffsetY;
+            if (!(20 < args.OffsetX
+                && args.OffsetX < bitmap.Width + 20
+                && 20 < args.OffsetY
+                && args.OffsetY < bitmap.Height + 20))
+                return;
+            x = args.OffsetX - 20;
+            y = args.OffsetY - 20;
             Hct hct = Hct.From(360 * x / bitmap.Width, 100, 100 * y / bitmap.Height);
             themeService.Seed = hct.ToInt();
         }
