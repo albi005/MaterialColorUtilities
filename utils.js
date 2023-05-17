dae817a9c4241d96824c97908da974a5b64ffe49
albi005 <me@alb1.hu>
2023-05-17 11:55:09 +0000

    Deploying to gh-pages from  @ f5cf055d068e0ad2a0544dbda9a536390fdb5d13 🚀

--- utils.js
+++ utils.js
@@ -1,3 +1,7 @@
 ﻿window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', ev => {
     DotNet.invokeMethod('Playground.Wasm', 'OnIsDarkChanged', ev.matches);
 });
+
+function postScheme(scheme) {
+    window.parent?.postMessage(scheme, '*');
+}
